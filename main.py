e1db854fb0bd8f83dfbaff626a680b7a4beb6f7b
Ульяна <113327834+uliat1@users.noreply.github.com>
2023-09-05 15:55:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ class CoffeeMachine:
         self.water = 400  # количество воды в мл
         self.milk = 540  # количество молока в мл
         self.coffee_beans = 120  # количество кофейных зерен в граммах
+        self.sugar = 50  # количество сахара в кубиках
         self.money = 550  # количество денег в рублях
         self.cups = 10  # количество стаканчиков
 
     def remaining(self):
-        print(f"The coffee machine has: {self.water} of water, {self.milk} of milk, {self.money} of money, {self.coffee_beans} of coffee beans, {self.cups} of cups")
+        print(f"The coffee machine has: {self.water} of water, {self.milk} of milk, {self.sugar} of sugar,{self.money} of money, {self.coffee_beans} of coffee beans, {self.cups} of cups")
 
     def buy(self):
         choice = input("What do you want to buy?\n1 - espresso\n2 - latte\n3 - cappuccino\n")
         if choice == "1":
-            self.make_coffee(250, 0, 16, 10, 1)
+            self.make_coffee(250, 0, 16, 0, 10, 1)
         elif choice == "2":
-            self.make_coffee(350, 75, 20, 25, 1)
+            self.make_coffee(350, 75, 20, 3, 25, 1)
         elif choice == "3":
-            self.make_coffee(200, 100, 12, 20, 1)
+            self.make_coffee(200, 100, 12, 2, 20, 1)
         else:
             print("Invalid choice!")
 
-    def make_coffee(self, water_needed, milk_needed, coffee_beans_needed, cost, cups_needed):
-        if self.water >= water_needed and self.milk >= milk_needed and self.coffee_beans >= coffee_beans_needed and self.cups >= cups_needed:
+    def make_coffee(self, water_needed, milk_needed, coffee_beans_needed, sugar_needed, cost, cups_needed):
+        if self.water >= water_needed \
+                and self.milk >= milk_needed \
+                and self.coffee_beans >= coffee_beans_needed \
+                and self.sugar >= sugar_needed \
+                and self.cups >= cups_needed:
             print("Making coffee! Enjoy your drink!")
             self.water -= water_needed
             self.milk -= milk_needed
@@ -35,6 +40,7 @@ class CoffeeMachine:
         self.water += int(input("How much water do you want to add?\n"))
         self.milk += int(input("How much milk do you want to add?\n"))
         self.coffee_beans += int(input("How much coffee beans do you want to add?\n"))
+        self.sugar += int(input("How much sugar do you want to add?\n"))
         self.money += int(input("How much money do you want to add?\n"))
         self.cups += int(input("How much cups do you want to add?\n"))
 
